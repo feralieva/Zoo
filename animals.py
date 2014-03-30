63477d4039f33c74051e247fca7c5dc2859b3ca3
smo93 <georgimpeev@gmail.com>
2014-03-30 18:23:57 +0300

    fixed the animals weight

--- animals.py
+++ animals.py
@@ -4,14 +4,21 @@ from random import randint
 
 class Animal():
     """docstring for Animal"""
-    def __init__(self, db_conn, species, age, name, gender, weight):
+    def __init__(self, db_conn, species, age, name, gender):
         self.species = species
         self.age = age
         self.name = name
         self.gender = gender
-        self.weight = weight
         self.db_conn = db_conn
-        
+
+        cursor = self.db_conn.cursor()
+        sql_a_to_w = 'SELECT weight_age_ratio, average_weight FROM animals WHERE species = ?'
+        sql_result = cursor.execute(sql_a_to_w, (self.species, )).fetchone()
+        age_to_weight = sql_result[0]
+        self.weight = self.age * age_to_weight
+        if self.weight > sql_result[1]:
+            self.weight = sql_result[1]
+
     def grow(self):
         self.age += 1
         sql_avarage = "SELECT average_weight, weight_age_ratio FROM animals WHERE species = ?"
@@ -19,7 +26,7 @@ class Animal():
         sql_result = cursor.execute(sql_avarage, (self.species,)).fetchone()
         if sql_result[0] > self.weight:
             self.weight += sql_result[1]
-    
+
     def eat(self):
         sql_avarage = "SELECT average_weight, food_weight_ratio FROM animals WHERE species = ?"
         cursor = self.db_conn.cursor()
