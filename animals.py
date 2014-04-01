933139c92fcf55e1ba0344c2b0f737c453fbbfdd
smo93 <georgimpeev@gmail.com>
2014-04-01 19:06:24 +0300

    changed '"' to ''' :D
    - also changed the death chance

--- animals.py
+++ animals.py
@@ -13,7 +13,8 @@ class Animal():
         self.db_conn = db_conn
 
         cursor = self.db_conn.cursor()
-        sql_a_to_w = 'SELECT weight_age_ratio, average_weight FROM animals WHERE species = ?'
+        sql_a_to_w = '''SELECT weight_age_ratio, average_weight FROM animals
+                        WHERE species = ?'''
         sql_result = cursor.execute(sql_a_to_w, (self.species, )).fetchone()
         age_to_weight = sql_result[0]
         self.weight = self.age * age_to_weight
@@ -30,26 +31,28 @@ class Animal():
 
     def grow(self):
         self.age += 1
-        sql_avarage = "SELECT average_weight, weight_age_ratio FROM animals WHERE species = ?"
+        sql_avarage = '''SELECT average_weight, weight_age_ratio FROM animals
+                         WHERE species = ?'''
         cursor = self.db_conn.cursor()
         sql_result = cursor.execute(sql_avarage, (self.species,)).fetchone()
         if sql_result[0] > self.weight:
             self.weight += sql_result[1]
 
     def eat(self):
-        sql_avarage = "SELECT average_weight, food_weight_ratio FROM animals WHERE species = ?"
+        sql_avarage = '''SELECT average_weight, food_weight_ratio FROM animals
+                         WHERE species = ?'''
         cursor = self.db_conn.cursor()
         sql_result = cursor.exeute(sql_avarage, (self.species,)).fetchone()
         if self.weight < sql_result[0]:
             self.weight += sql_result[1]
 
     def die(self):
-        sql_avarage = "SELECT life_expectancy FROM animals WHERE species = ?"
+        sql_avarage = 'SELECT life_expectancy FROM animals WHERE species = ?'
         cursor = self.db_conn.cursor()
         sql_result = cursor.execute(sql_avarage, (self.species,)).fetchone()
-        #rand_koef = randint(1,3)
-        chance = 100//(sql_result[0] - self.age/12 )
+        chance = self.age / sql_result[0]
         rand_die = randint(1,100)
+
         if rand_die <= chance:
             return True
         else:
