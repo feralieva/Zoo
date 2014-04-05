0e3457a9d10d53466788fb09c79470e63ccd6627
feralieva <t.feralieva@gmail.com>
2014-04-05 17:32:55 +0300

    change

--- test_zoo_file_adapter.py
+++ test_zoo_file_adapter.py
@@ -1,5 +1,6 @@
 import unittest
 import sqlite3
+from animals import Animal
 from zoo_file_adapter import ZooFileAdapter
 from zoo import Zoo
 from subprocess import call
@@ -27,6 +28,24 @@ class ZooFileAdapterTest(unittest.TestCase):
 
         expected = ['pe6o : tiger, 10, 120.0']
         self.assertEqual(expected, zoo.see_animals())
+
+    def test_load(self):
+        zoo = Zoo(self.db_conn, 10, 10000)
+        zoo.accomodate_animal('tiger', 'pe6o', 10, 'male')
+        zoo_adapter = ZooFileAdapter(self.db_conn, zoo)
+        zoo_adapter.save()
+        new_zoo = Zoo(self.db_conn, 0, 0)
+        new_zoo_adapter = ZooFileAdapter(self.db_conn, new_zoo)
+
+        self.assertEqual(True, new_zoo_adapter.load(1))
+        animal = Animal(self.db_conn, 'tiger', 10, 'pe6o', 'male')
+        self.assertEqual(10, new_zoo.capacity)
+        self.assertEqual(10000, new_zoo.budget)
+
+        expected = ['pe6o : tiger, 10, 120.0']
+        self.assertEqual(expected, zoo.see_animals())
+
+
 
     def tearDown(self):
         self.db_conn.commit()
