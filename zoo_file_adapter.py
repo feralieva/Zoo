4a6ec10dcd02ecbf62c4cdff20052ff7669206b4
feralieva <t.feralieva@gmail.com>
2014-04-05 17:34:37 +0300

    implemented the load method

--- zoo_file_adapter.py
+++ zoo_file_adapter.py
@@ -11,8 +11,8 @@ class ZooFileAdapter():
         self._ensure_table_exists()
 
     def _ensure_table_exists(self):
-        create_table = '''create table if not exists
-                          zoos(id integer primry key,
+        create_table = '''CREATE TABLE IF NOT EXISTS
+                          zoos(id INTEGER PRIMARY KEY,
                                 capacity int,
                                 budget int)'''
         cursor = self.db_conn.cursor()
@@ -20,12 +20,12 @@ class ZooFileAdapter():
 
     def save(self):
         if self.zoo.get_id() == -1:
-            add_zoo = 'insert into zoos(capacity, budget) VALUES(?, ?)'
+            add_zoo = 'INSERT INTO zoos(capacity, budget) VALUES(?, ?)'
             cursor = self.db_conn.cursor()
             cursor.execute(add_zoo, (self.zoo.capacity, self.zoo.budget))
             self.zoo.set_id(cursor.lastrowid)
         else:
-            update_zoo = 'update zoos set budget=?, capacity=?'
+            update_zoo = 'UPDATE zoos SET budget=?, capacity=?'
             cursor = self.db_conn.cursor()
             cursor.execute(update_zoo, (self.zoo.budget, self.zoo.capacity))
 
@@ -34,4 +34,27 @@ class ZooFileAdapter():
             animal_adapter.save()
 
     def load(self, load_id=-1):
-        pass
+        req_id = -1
+        if self.zoo.get_id() > 0:
+            req_id = self.zoo.get_id()
+        elif load_id > 0:
+            req_id = load_id
+        else:
+            return False
+
+        get_zoo = 'SELECT * FROM zoos WHERE id=?'
+        cursor = self.db_conn.cursor()
+        result = cursor.execute(get_zoo, (req_id,)).fetchone()
+
+        self.zoo.capacity = result[1]
+        self.zoo.budget = result[2]
+        
+        animal_from_zoo = 'SELECT id FROM zoo_animals WHERE id=?'
+        animal_cursor = self.db_conn.cursor()
+        anim_result = animal_cursor.execute(animal_from_zoo, (req_id,))
+        for row in anim_result:
+            animal = Animal(self.db_conn, 'tiger', 0, '', '')
+            id_animal = animal.set_id(row[0])
+            animal_adapter = AnimalFileAdapter(self.db_conn, animal)
+            animal_adapter.load(id_animal)
+        return True
